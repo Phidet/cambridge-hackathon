851af54617e8b603b2273de32b393692200a168e
Jiameng Gao <19616679+jiamenguk@users.noreply.github.com>
2024-12-06 20:01:58 +0000

    added agent hook

--- react-hook/hook.ts
+++ react-hook/hook.ts
@@ -1,15 +1,17 @@
-/* Sillwood Technologies Limited, 2023 */
+/* Neuphonic Limited, 2024 */
 
 import { useEffect, useRef, useState } from 'react';
 
 export type WebSocketResponse = {
   data: {
-    audio: string;
     text: string;
+    audio: string;
     sampling_rate: number | null;
   };
-  version: string;
-  timestamp: string;
+  agent_id: string;
+  conversation_id: string;
+  type: 'user' | 'assistant';
+  completed?: boolean;
 };
 
 export type Message = {
@@ -61,6 +63,163 @@ function base64ToArrayBuffer(base64: string) {
   const byteArray = new Uint8Array(byteArrays).buffer;
 
   return byteArray;
+}
+
+export function useAgent(
+  agentId: string,
+  languageId: LanguageId,
+  bufferDelay: number = 0.1
+) {
+  const ws = useRef<WebSocket>();
+  const [connect, setConnect] = useState(false);
+  const mediaStream = useRef<MediaStream | null>(null);
+  const [status, setStatus] = useState('Not connected');
+  const [transcript, setTranscript] = useState<Message[]>([]);
+  const [speaking, setSpeaking] = useState(false);
+
+  const stopRecording = () => {
+    if (ws.current && ws.current.readyState == ws.current.OPEN)
+      ws.current.close();
+    if (mediaStream.current) {
+      mediaStream.current
+        .getTracks()
+        .forEach((track) => track.stop());
+    }
+    setStatus('Not connected');
+    setConnect(false);
+  };
+
+  useEffect(() => {
+    if (!agentId || !connect) {
+      return;
+    }
+
+    let duration = 0;
+
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: {
+          sampleRate: 16000,
+          channelCount: 1,
+          echoCancellation: true,
+          noiseSuppression: true,
+          autoGainControl: true,
+        },
+      })
+      .then((stream) => {
+        mediaStream.current = stream;
+        ws.current = new WebSocket(
+          `wss://eu-west-1.api.neuphonic.com/listen/${agentId}/${languageId}?` +
+            new URLSearchParams({
+              api_key: 'YOUR API KEY HERE',
+            })
+        );
+        var audioCtx = new AudioContext();
+        const mediaRecorder = new MediaRecorder(stream);
+        ws.current!.onopen = () => {
+          setStatus('Connected');
+          mediaRecorder.addEventListener(
+            'dataavailable',
+            async (event) => {
+              if (
+                event.data.size > 0 &&
+                ws.current!.readyState == 1
+              ) {
+                ws.current!.send(event.data);
+              }
+            }
+          );
+          mediaRecorder.start(100);
+        };
+
+        ws.current!.onmessage = (message: MessageEvent<string>) => {
+          const received = JSON.parse(
+            message.data
+          ) as WebSocketResponse;
+          if (received) {
+            if (received.data && received.data.text !== null) {
+              const text = received.data.text;
+              setTranscript((prev) => {
+                if (received.type === prev[0]?.role) {
+                  prev[0].text = prev[0].text + ` ${text}`;
+                  prev[0].completed = received.completed;
+                  return [...prev];
+                } else if (text === '') {
+                  return prev;
+                } else {
+                  return [
+                    {
+                      role: received.type,
+                      text: text,
+                      completed: received.completed,
+                    },
+                    ...prev,
+                  ];
+                }
+              });
+            }
+            if (received.data && received.data.audio !== null) {
+              const byteArray = base64ToArrayBuffer(
+                received.data.audio
+              );
+
+              var track = audioCtx.createBufferSource();
+              track.buffer = createBuffer(
+                byteArray,
+                audioCtx,
+                received.data.sampling_rate ?? 44100
+              );
+
+              track.connect(audioCtx.destination);
+              if (duration == 0)
+                duration = audioCtx.currentTime + 0.0;
+              duration = Math.max(audioCtx.currentTime, duration);
+              track.start(duration);
+              duration += track.buffer.duration;
+              setSpeaking(true);
+              track.onended = () => {
+                if (audioCtx.currentTime >= duration) {
+                  setSpeaking(false);
+                }
+              };
+            }
+            if (received.type === 'user') {
+              // Reset audio duration for new sentences
+              void audioCtx.close();
+              duration = 0;
+              setSpeaking(false);
+              audioCtx = new AudioContext();
+            }
+          }
+        };
+        ws.current.onclose = () => {
+          console.log({
+            event: 'onclose',
+          });
+        };
+
+        ws.current.onerror = (error: Event) => {
+          setStatus('Error');
+          console.log({
+            event: 'onerror',
+            error,
+          });
+        };
+      });
+
+    return () => {
+      stopRecording();
+    };
+  }, [agentId, connect]);
+
+  return {
+    transcript,
+    status,
+    connect,
+    speaking,
+    setConnect,
+    stopRecording,
+  };
 }
 
 export function useTTS(
